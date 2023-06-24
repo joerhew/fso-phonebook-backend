1103929cd67f1de422afbdbb3b9b730f3ea80e09
joerhew <joerhew@gmail.com>
2023-06-24 14:58:11 -0400

    added production build of frontend

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fso-phonebook-backend
 Full Stack Open - building the backend for the phonebook app
+
+Link to online application: https://fso-phonebook-joerhew.fly.dev/
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors')
 
 morgan.token('body', (req, res) => {
   return req.method === 'POST'
@@ -8,6 +9,8 @@ morgan.token('body', (req, res) => {
     : '';
 });
 
+app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
@@ -97,7 +100,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
